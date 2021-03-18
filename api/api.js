d98dae2d2caaa911827975229c8b9b2c52a5138d
Никита Корнилов <nikkorfed@gmail.com>
2021-03-18 12:33:46 +0500

    Изменён способ подставления токена в запросы к API.

--- api/api.js
+++ api/api.js
@@ -2,12 +2,11 @@ import axios from "axios";
 
 import authStorage from "../auth/storage";
 
-let api;
-
-(async () => {
-  // api = axios.create({ baseURL: "http://taxi.adboard.online/api" });
-  api = axios.create({ baseURL: "http://10.66.80.88:1123/api" });
-  api.defaults.headers.common["X-Auth-Token"] = await authStorage.getToken();
-})();
+// const api = axios.create({ baseURL: "http://taxi.adboard.online/api" });
+const api = axios.create({ baseURL: "http://10.66.80.88:1123/api" });
+api.interceptors.request.use(async (config) => {
+  config.headers["X-Auth-Token"] = await authStorage.getToken();
+  return config;
+});
 
 export default api;
