8954d200b4e939ad426aa1c50553ed6c900bccb0
Никита Корнилов <nikkorfed@gmail.com>
2021-04-05 13:45:48 +0500

    Доработана обработка состояния в текстовых полях.

--- components/Inputs.js
+++ components/Inputs.js
@@ -5,18 +5,26 @@ import { TextInputMask } from "react-native-masked-text";
 
 import styles from "../styles";
 
-export let TextInput = ({ style, state, ...props }) => {
+let getState = (state) => {
   let value, set;
 
   if (Array.isArray(state)) [value, set] = state;
   else ({ value, set } = state);
+
+  return [value, set];
+};
+
+export let TextInput = ({ style, state, ...props }) => {
+  let [value, set] = getState(state);
 
   props.style = [styles.input, style];
 
   return <Input value={value} onChangeText={set} {...props} />;
 };
 
-export let PhoneInput = ({ style, state: [phone, setPhone], ...props }) => {
+export let PhoneInput = ({ style, state, ...props }) => {
+  let [value, set] = getState(state);
+
   props.style = [styles.input, style];
   props.type = "custom";
   props.options = { mask: "+7 999 999-99-99" };
@@ -24,35 +32,36 @@ export let PhoneInput = ({ style, state: [phone, setPhone], ...props }) => {
   props.placeholder = "Телефон";
 
   let editPhone = (text) => {
-    text == "" || text == "+" ? setPhone("+7") : setPhone(text);
+    text == "" || text == "+" ? set("+7") : set(text);
     text.length == 16 ? Keyboard.dismiss() : null;
   };
 
   props.onFocus = () => phone == "" && editPhone("");
-  props.onChangeText = editPhone;
 
-  return <TextInputMask value={phone} onChangeText={setPhone} {...props} />;
+  return <TextInputMask value={value} onChangeText={editPhone} {...props} />;
 };
 
-export let PasswordInput = ({ style, state: [password, setPassword], ...props }) => {
+export let PasswordInput = ({ style, state, ...props }) => {
+  let [value, set] = getState(state);
+
   props.style = [styles.input, style];
   props.secureTextEntry = true;
   props.placeholder = "Пароль";
 
-  return <Input value={password} onChangeText={setPassword} {...props} />;
+  return <Input value={value} onChangeText={set} {...props} />;
 };
 
-export let CodeInput = ({ style, state: [code, setCode], ...props }) => {
+export let CodeInput = ({ style, state, ...props }) => {
+  let [value, set] = getState(state);
+
   props.style = [styles.input, style];
   props.keyboardType = "number-pad";
   props.placeholder = "Одноразовый код";
 
   let editCode = (text) => {
-    setCode(text);
+    set(text);
     text.length == 6 ? Keyboard.dismiss() : null;
   };
 
-  props.onChangeText = editCode;
-
-  return <Input value={code} onChangeText={setCode} {...props} />;
+  return <Input value={value} onChangeText={editCode} {...props} />;
 };
