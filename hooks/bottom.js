7f567c16184791dc58879d3771db5ac01c3db488
Никита Корнилов <nikkorfed@gmail.com>
2021-04-08 13:59:39 +0500

    Доработано поведение и анимация всплывающего окна.

--- hooks/bottom.js
+++ hooks/bottom.js
@@ -2,19 +2,22 @@ import { useWindowDimensions } from "react-native";
 import { useSharedValue, useAnimatedStyle, useAnimatedGestureHandler, withSpring, interpolate, Extrapolate } from "react-native-reanimated";
 
 export default () => {
-  const start = 40;
+  const offset = 40;
   const config = { damping: 80, overshootClamping: true, restDisplacementThreshold: 0.1, restSpeedThreshold: 0.1, stiffness: 500 };
 
   const window = useWindowDimensions();
   const pos = useSharedValue(window.height);
   const opacity = useSharedValue(0);
 
-  const view = useAnimatedStyle(() => ({ top: pos.value }));
-  const overlay = useAnimatedStyle(() => ({ display: opacity.value > 0 ? "flex" : "none", opacity: opacity.value }));
+  const view = useAnimatedStyle(() => ({ top: interpolate(pos.value, [-500, offset, window.height], [0, offset, window.height]) }));
+  const overlay = useAnimatedStyle(() => ({
+    display: pos.value < window.height ? "flex" : "none",
+    opacity: interpolate(pos.value, [0, offset, window.height], [0.5, 0.5, 0]),
+  }));
 
   const open = () => {
     "worklet";
-    pos.value = withSpring(start, config);
+    pos.value = withSpring(offset, config);
     opacity.value = withSpring(0.5, config);
   };
   const close = () => {
@@ -25,7 +28,7 @@ export default () => {
 
   const gestureHandler = useAnimatedGestureHandler({
     onStart: (_, ctx) => (ctx.start = pos.value),
-    onActive: (event, ctx) => (pos.value = ctx.start + event.translationY < start ? start : ctx.start + event.translationY),
+    onActive: (event, ctx) => (pos.value = ctx.start + event.translationY),
     onEnd: (event, _) => (pos.value > 150 ? close() : open()),
   });
 
