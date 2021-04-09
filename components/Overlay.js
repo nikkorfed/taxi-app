fccb99e60c121ffd732b8b7fbb27ed018f213529
Никита Корнилов <nikkorfed@gmail.com>
2021-04-09 17:00:17 +0500

    Доработаны иконки зума.

--- components/Overlay.js
+++ components/Overlay.js
@@ -2,7 +2,7 @@ import React from "react";
 import { StyleSheet, View, Text, TouchableOpacity, useWindowDimensions } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-import { AntDesign, FontAwesome, FontAwesome5, MaterialIcons } from "@expo/vector-icons";
+import { AntDesign, Entypo, FontAwesome, FontAwesome5, MaterialIcons } from "@expo/vector-icons";
 
 export default ({ navigation, zoomIn, zoomOut, toCurrentLocation, sheets }) => {
   const center = useWindowDimensions().height / 2;
@@ -13,14 +13,16 @@ export default ({ navigation, zoomIn, zoomOut, toCurrentLocation, sheets }) => {
       <TouchableOpacity style={[screen.menuButton, { top: insets.top + 10 }]} onPress={navigation.openDrawer} activeOpacity={0.8}>
         <MaterialIcons name="menu" size={20} color="#333" />
       </TouchableOpacity>
-      <TouchableOpacity style={[screen.zoomButton, { top: center - (40 + 15 / 2) }]} onPress={zoomIn} activeOpacity={0.8}>
-        <FontAwesome5 name="plus" size={20} color="#333" />
-      </TouchableOpacity>
-      <TouchableOpacity style={[screen.zoomButton, { top: center + 15 / 2 }]} onPress={zoomOut} activeOpacity={0.8}>
-        <FontAwesome5 name="minus" size={20} color="#333" />
-      </TouchableOpacity>
-      <TouchableOpacity style={[screen.locationButton, { top: center + 15 / 2 + 40 + 15 }]} onPress={toCurrentLocation} activeOpacity={0.8}>
-        <FontAwesome name="location-arrow" size={20} color="#333" />
+      <View style={[screen.zoomButtons, { top: center - 40 }]}>
+        <TouchableOpacity style={screen.zoomInButton} onPress={zoomIn} activeOpacity={0.8}>
+          <Entypo name="plus" size={20} color="#333" />
+        </TouchableOpacity>
+        <TouchableOpacity style={screen.zoomOutButton} onPress={zoomOut} activeOpacity={0.8}>
+          <Entypo name="minus" size={20} color="#333" />
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity style={[screen.locationButton, { top: center + 40 + 15 }]} onPress={toCurrentLocation} activeOpacity={0.8}>
+        <FontAwesome name="location-arrow" size={18} color="#333" />
       </TouchableOpacity>
       <View style={[screen.destination, { paddingBottom: insets.bottom > 0 ? insets.bottom : screen.destination.padding }]}>
         <TouchableOpacity style={screen.destinationInput} onPress={sheets.route.open} activeOpacity={0.8}>
@@ -46,18 +48,33 @@ const screen = StyleSheet.create({
     shadowRadius: 15,
     shadowOpacity: 0.15,
   },
-  zoomButton: {
-    justifyContent: "center",
-    alignItems: "center",
+  zoomButtons: {
     position: "absolute",
     right: 15,
     borderRadius: 10,
-    height: 40,
-    width: 40,
-    backgroundColor: "white",
     shadowColor: "black",
     shadowRadius: 15,
     shadowOpacity: 0.15,
+  },
+  zoomInButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderBottomWidth: 1,
+    borderColor: "#eee",
+    height: 40,
+    width: 40,
+    backgroundColor: "white",
+  },
+  zoomOutButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    height: 40,
+    width: 40,
+    backgroundColor: "white",
   },
   locationButton: {
     justifyContent: "center",
